c12f7de87ab366f235eb44f45d7485e09035dda3
muhnasih <muhnasih06@gmail.com>
2025-09-04 14:58:11 +0700

    tugas minggu 2

---
+++ Codelab02/src/latevariable.dart
@@ -0,0 +1,8 @@
+void main() {
+  // Variabel late (tidak langsung diberi nilai)
+  late String university;
+
+  // Inisialisasi dilakukan kemudian
+  university = "Politeknik Negeri Malang";
+  print("Kuliah di: $university");
+}
