5047660e8d88afd5ae63b4c7c60b4320c8ec8b1d
muhnasih <muhnasih06@gmail.com>
2025-09-11 16:08:28 +0700

    ..

--- WEEK3/src/Praktikum1.dart
+++ WEEK3/src/Praktikum1.dart
@@ -1,5 +1,5 @@
 void main() {
-  // Bagian if-else
+  
   String test = "test2";
 
   if (test == "test1") {
@@ -14,29 +14,28 @@ void main() {
     print("Test2 again");
   }
 
-  // Tambahan kode if
   String test2 = "true";
   if (test2 == "true") {
     print("Kebenaran");
   }
 
-  // Perulangan while
+  
   int counter = 0;
   while (counter < 33) {
     print(counter);
     counter++;
   }
 
-  // Perulangan do-while
+  
   do {
   print(counter);
   counter++;
 } while (counter < 77);
 
-  // Perulangan for dengan break & continue
+
   for (int index = 10; index < 27; index++) {
-    if (index == 21) break;                // berhenti jika index = 21
-    else if (index > 1 && index < 7) continue; // lewati index 2–6
+    if (index == 21) break;                
+    else if (index > 1 && index < 7) continue; 
     print(index);
   }
 }
